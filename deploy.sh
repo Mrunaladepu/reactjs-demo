676f035cf7f00c840ed635b826f464c8608eb202
Ubuntu <ubuntu@ip-172-31-42-109.ap-south-1.compute.internal>
2023-07-10 12:22:42 +0000

    commited

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Stop script on first error
+set -e
+
+IMAGE_NAME="mrunaladepu/reactappproject"
+IMAGE_TAG=$(git rev-parse --short HEAD) # first 7 characters of the current commit hash
+
+echo "Building Docker image ${IMAGE_NAME}:${IMAGE_TAG}, and tagging as latest"
+docker build -t "${IMAGE_NAME}:${IMAGE_TAG}" .
+docker tag "${IMAGE_NAME}:${IMAGE_TAG}" "${IMAGE_NAME}:latest"
+DOCKER_USERNAME="mrunaladepu"
+DOCKER_PASSWORD="1Munna1101$"
+echo "Authenticating and pushing image to Docker Hub"
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+docker push "${IMAGE_NAME}:${IMAGE_TAG}"
+docker push "${IMAGE_NAME}:latest"
